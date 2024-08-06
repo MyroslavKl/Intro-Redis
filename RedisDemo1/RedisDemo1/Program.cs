882984796c37dfaf101451a57626f9c1c930ee42
Myroslav <muroslavkl@gmail.com>
2024-08-06 19:31:52 +0300

    Redis first touch

---
+++ RedisDemo1/RedisDemo1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using RedisDemo1;
+
+var cache = RedisHelper.Database;
+await cache.StringSetAsync("Author","Agata Kristi");
+var value = await cache.StringGetAsync("Author");
+Console.WriteLine(value);
---
+++ RedisDemo1/RedisDemo1/RedisHelper.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace RedisDemo1;
+
+public class RedisHelper
+{
+    private static IDatabase redisDatabase;
+
+    public static IDatabase Database
+    {
+        get
+        {
+            return redisDatabase;
+        }
+    }
+
+    static RedisHelper()
+    {
+        var connection = ConnectionMultiplexer.Connect("localhost:6379");
+        redisDatabase = connection.GetDatabase();
+    }
+}
